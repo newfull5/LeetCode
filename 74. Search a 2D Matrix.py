eff84eb4d555a3c8ca20c904d32a1cb3f8db54f3
Sae_Chan_Oh <dhtocks@naver.com>
2021-01-20 15:15:49 +0900

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,3 +1,5 @@
+"""
+#faster than 25.34%
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for i in range(len(matrix)):
@@ -5,3 +7,14 @@ class Solution:
                 if matrix[i][j] == target:
                     return True
         return False
+"""
+
+#faster than 62.63%
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            if (target <= matrix[i][-1]):
+                for j in range(len(matrix[0])):
+                    if target == matrix[i][j]:
+                        return True
+                return False
