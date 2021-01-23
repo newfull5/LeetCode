cdda0d2a88e8d6525d37e3c09790f3aa871625fa
Sae_Chan_Oh <dhtocks@naver.com>
2021-01-23 23:36:37 +0900

    Create 118. Pascal's Triangle.py

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        arr = []
+
+        for i in range(1,numRows+1):
+            temp = [0] * i
+            temp[0] = 1
+            temp[-1] = 1
+            arr.append(temp)
+
+        for i in range(1,len(arr)-1):
+            for j in range(0, len(arr[i])-1):
+                arr[i+1][j+1] = (arr[i][j] + arr[i][j+1])
+                
+        return arr
