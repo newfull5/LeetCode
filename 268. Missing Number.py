9ed9df2fc0fd0b702e2b460cfc3a71b57d2df1ec
Sae_Chan_Oh <dhtocks@naver.com>
2021-01-25 10:48:54 +0900

    Update 268. Missing Number.py

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,5 +1,14 @@
+"""
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         for i in range(max(nums)+2):
             if i not in nums:
                 return i
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = max(nums)
+        answer = (n*(n + 1) // 2) - sum(nums)
+        if answer == 0 and 0 in nums:
+            return n+1
+        return answer
