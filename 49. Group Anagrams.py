eb4c63836c8472e0137810709f706b0bb73bfd6e
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-01 15:01:41 +0900

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,3 +1,5 @@
+"""
+#Runtime: 100 ms, faster than 59.14% Memory Usage: 17.1 MB, less than 92.97%
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         diction = dict()
@@ -10,3 +12,16 @@ class Solution:
                 diction[temp].append(string)
 
         return list(diction.values())
+"""
+#Runtime: 220 ms, faster than 5.59% Memory Usage: 17.9 MB, less than 45.76%    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+
+        anagrams = defaultdict(list)
+
+        for word in strs:
+
+            anagrams[''.join(sorted(word))].append(word)
+
+        return anagrams.values()
