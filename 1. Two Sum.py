23b229300b53582de7faf5fc9a323fde492aae9d
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-01 15:20:50 +0900

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,3 +1,6 @@
+"""
+#Runtime: 44 ms, faster than 85.74% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.4 MB, less than 46.77% of Python3 online submissions for Two Sum.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
@@ -5,3 +8,13 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return ([i,j])
+"""
+             
+#Runtime: 48 ms, faster than 65.32% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.4 MB, less than 46.77% of Python3 online submissions for Two Sum.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums:
+                if i != nums.index(target - nums[i]):
+                    return [i, nums.index(target - nums[i])]
