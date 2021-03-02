e1788d80c380f76ffa0b7e669d03c02699399b4c
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-02 23:15:46 +0800

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -1,5 +1,8 @@
+# Runtime: 36 ms, faster than 89.98% of Python3 online submissions for Permutations.
+# Memory Usage: 14.3 MB, less than 90.99% of Python3 online submissions for Permutations.
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations
+        from itertools import permutations as P
 
-        return list(map(lambda x: list(x), list(permutations(nums))))
+        return list(map(lambda x: list(x), P(nums)))
