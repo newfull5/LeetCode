22b0b958510dbeb0fa72e5f623cdd119ce5218b5
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-02 22:15:11 +0800

    Update 771. Jewels and Stones.py

--- 771. Jewels and Stones.py
+++ 771. Jewels and Stones.py
@@ -1,3 +1,6 @@
+#Runtime: 32 ms, faster than 63.11% of Python3 online submissions for Jewels and Stones.
+#Memory Usage: 14.3 MB, less than 13.91% of Python3 online submissions for Jewels and Stones.
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         answer = 0
