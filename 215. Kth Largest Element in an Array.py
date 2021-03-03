d4b0f48e3c0a1923b90fbe0aac10aae446cb2a1b
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-03 22:31:43 +0800

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,7 +1,24 @@
+"""
 # Runtime: 64 ms, faster than 72.48% of Python3 online submissions for Kth Largest Element in an Array.
 # Memory Usage: 15.1 MB, less than 44.88% of Python3 online submissions for Kth Largest Element in an Array.
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return sorted(nums, reverse=True)[k-1]
-        
+"""
+
+# Runtime: 64 ms, faster than 72.48% of Python3 online submissions for Kth Largest Element in an Array.
+# Memory Usage: 15 MB, less than 93.30% of Python3 online submissions for Kth Largest Element in an Array.
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        from heapq import heappop, heapify
+
+        nums = list(map(lambda x: x*-1, nums))
+
+        heapify(nums)
+
+        for _ in range(k-1):
+            heappop(nums)
+
+        return heappop(nums) * -1        
