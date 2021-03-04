cf19d315e359a20de47ca4cd92daa58d7c907d79
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-04 13:21:08 +0800

    Update 240. Search a 2D Matrix II.py

--- 240. Search a 2D Matrix II.py
+++ 240. Search a 2D Matrix II.py
@@ -1,3 +1,4 @@
+"""
 # Runtime: 152 ms, faster than 97.87% of Python3 online submissions for Search a 2D Matrix II.
 # Memory Usage: 20.6 MB, less than 31.15% of Python3 online submissions for Search a 2D Matrix II.
 
@@ -7,3 +8,11 @@ class Solution:
             if target in arr:
                 return True
         return False
+"""
+    
+# Runtime: 168 ms, faster than 52.23% of Python3 online submissions for Search a 2D Matrix II.
+# Memory Usage: 20.7 MB, less than 31.15% of Python3 online submissions for Search a 2D Matrix II.
+    
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return any([target in row for row in matrix])
