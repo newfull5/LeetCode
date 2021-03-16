fe7e2d36a61f97f5c4c81d1193aeadc181ef9c4b
Sae_Chan_Oh <dhtocks@naver.com>
2021-03-16 21:33:16 +0900

    Update 929. Unique Email Addresses.py

--- 929. Unique Email Addresses.py
+++ 929. Unique Email Addresses.py
@@ -1,3 +1,4 @@
+"""
 # Runtime: 48 ms, faster than 83.81% of Python3 online submissions for Unique Email Addresses.
 # Memory Usage: 14.4 MB, less than 62.51% of Python3 online submissions for Unique Email Addresses.
 
@@ -16,3 +17,22 @@ class Solution:
             emails[i] = local + '@' + domain
 
         return len(set(emails))
+"""
+
+# Runtime: 48 ms, faster than 83.81% of Python3 online submissions for Unique Email Addresses.
+# Memory Usage: 14.5 MB, less than 33.59% of Python3 online submissions for Unique Email Addresses.
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        
+        def make(email):
+            local, domain = email.split('@')
+
+            if '+' in email:
+                local = local[:local.index('+')]
+
+            local = local.replace('.','')
+
+            return local + '@' + domain
+
+        return len(set(map(make, emails)))
