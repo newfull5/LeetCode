0fbc2d4e03483c0154314812eee7a5d495ffdd80
SaeChan_Oh <dhtocks@naver.com>
2025-12-01 10:01:40 +0900

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,13 +1,19 @@
+
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         length = len(digits)
+        
+#         digits = map(str, digits)
+#         digits = list(str(int(''.join(digits)) + 1))
+        
+#         while len(digits) != length:
+#             if len(digits) >= length:
+#                 break
+#             digits = [0] + digits
+            
+#         return digits
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        length = len(digits)
-        
-        digits = map(str, digits)
-        digits = list(str(int(''.join(digits)) + 1))
-        
-        while len(digits) != length:
-            if len(digits) >= length:
-                break
-            digits = [0] + digits
-            
-        return digits
+        return [int(t) for t in str(int(''.join([str(digit) for digit in digits])) + 1)]
