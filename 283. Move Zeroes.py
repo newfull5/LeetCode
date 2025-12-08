f85d80e4f7e71c06e20df6a31c26f5d612aa95d0
SaeChan_Oh <dhtocks@naver.com>
2025-12-08 13:05:49 +0900

    Create 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda x: x==0)
